d48ecbb4362be970f2481298b79ba5f9ab683429
wenvit <wendyvit@icloud.com>
2025-03-02 10:33:26 -0600

    add code to handle rules for spock & lizard

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,8 @@
-
+# Add bonus feature:  rock-paper-scissors-spock-lizard
 
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
 def prompt(message):
     print(f'==> {message}')
@@ -10,19 +10,32 @@ def prompt(message):
 def display_winner(player, computer):
     prompt(f'You chose {player_choice}. '
            f'The computer chose {computer_choice}.')
-
     if ((player == 'rock' and computer == 'scissors') or
        (player == 'paper' and computer == 'rock') or
-       (player == 'scissors' and computer == 'paper')):
+       (player == 'scissors' and computer == 'paper') or
+       (player == 'rock' and computer == 'lizard') or
+       (player == 'lizard' and computer == 'spock') or
+       (player == 'spock' and computer == 'scissors') or
+       (player == 'scissors' and computer == 'lizard') or
+       (player == 'lizard' and computer == 'paper') or
+       (player == 'paper' and computer == 'spock') or
+       (player == 'spock' and computer == 'rock')):
         prompt('You win!')
-    elif ((player == 'rock' and computer == 'paper') or
-          (player == 'paper' and computer == 'scissors') or
-          (player == 'scissors' and computer == 'rock')):
+    elif ((computer == 'rock' and player == 'scissors') or
+       (computer == 'paper' and player == 'rock') or
+       (computer == 'scissors' and player == 'paper') or
+       (computer == 'rock' and player == 'lizard') or
+       (computer == 'lizard' and player == 'spock') or
+       (computer == 'spock' and player == 'scissors') or
+       (computer == 'scissors' and player == 'lizard') or
+       (computer == 'lizard' and player == 'paper') or
+       (computer == 'paper' and player == 'spock') or
+       (computer == 'spock' and player == 'rock')):
         prompt('Computer wins!')
     else:
         prompt("It's a tie!")
 
-prompt("Let's play Rock-Paper-Scissors!")
+prompt("Let's play Rock-Paper-Scissors-Spock-Lizard!")
 
 while True:
 
